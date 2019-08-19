3e6e79df2bc70b092c4bce311e43cc65a21fc582
Inna Mon <inna.mon@gmail.com>
2019-08-19 16:23:57 -0500

    Updates

---
+++ precache-manifest.35ea5cd24c47be043e9837f505d09278.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "05193151b03f80569a5a",
+    "url": "/robofriends/static/js/main.ed70b29d.chunk.js"
+  },
+  {
+    "revision": "08bb38ab1cc3d09ad245",
+    "url": "/robofriends/static/js/2.4b047d81.chunk.js"
+  },
+  {
+    "revision": "05193151b03f80569a5a",
+    "url": "/robofriends/static/css/main.f5936238.chunk.css"
+  },
+  {
+    "revision": "08bb38ab1cc3d09ad245",
+    "url": "/robofriends/static/css/2.38fb86b4.chunk.css"
+  },
+  {
+    "revision": "c62d05659812ef2fac8b628ac3496f0a",
+    "url": "/robofriends/index.html"
+  }
+];
